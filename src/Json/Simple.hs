29ed3bcf13e0e84c0e69e451719ac4345a6a2a50
LitFill <marrazzy54@gmail.com>
2025-02-26 00:10:59 +0700

    "todo: add_commit_comment"

--- src/Json/Simple.hs
+++ src/Json/Simple.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DerivingVia #-}
+
 module Json.Simple where
 
-import Data.Kind       (Type)
-import Data.Map.Strict (Map)
-import Fmt             ((+|), (|+))
+import Data.Kind           (Type)
+import Data.Map.Strict     (Map)
+import Fmt
 import Control.Applicative (Alternative (..))
+import Data.Char           (isDigit, isSpace)
+import Text.Read           (readEither)
+import GHC.Generics        (Generic)
 
--- import Data.Map.Strict qualified as Map
+import Data.Map.Strict qualified as Map
 
 data JsonValue :: Type where
     JsonNull   :: JsonValue
@@ -15,11 +20,34 @@ data JsonValue :: Type where
     JsonString :: String      -> JsonValue
     JsonArray  :: [JsonValue] -> JsonValue
     JsonObject :: Map String JsonValue -> JsonValue
-    deriving (Show, Eq)
+    deriving stock (Show, Eq, Generic)
+
+instance Buildable JsonValue where
+    build JsonNull = "null"
+    build (JsonBool True) = "true"
+    build (JsonBool False) = "false"
+    build (JsonNumber num) = build num
+    build (JsonString str) = build str
+    build (JsonArray list) = listF list
+    build (JsonObject obj) = listF $ map buildPair (Map.toList obj)
+    {-# INLINE build #-}
+
+buildPair
+    :: (Buildable a, Buildable b)
+    => (a, b) -> Builder
+buildPair (a, b) = "("+| a |+", "+| b |+")"
 
 newtype Result :: Type -> Type where
     Result :: { getEither :: Either String a } -> Result a
-    deriving (Show)
+    deriving (Generic)
+
+instance {-# Overlappable #-} Buildable a => Show (Result a) where
+    show = fmt . genericF
+
+instance (Buildable a, Buildable b) => Show (Result (a, b)) where
+    show r = case getEither r of
+        Right pair -> show (Result (Right (buildPair pair)))
+        Left err -> err
 
 newtype Parser :: Type -> Type where
     Parser ::
@@ -54,6 +82,13 @@ instance Alternative Result where
     Result (Right a) <|> _ = Result (Right a)
     Result (Left  _) <|> b = b
 
+instance Monad Result where
+  (>>=) :: Result a -> (a -> Result b) -> Result b
+  Result e1 >>= f = Result $ e1 >>= getEither . f
+
+failR :: String -> Result a
+failR = Result . Left
+
 instance Alternative Parser where
     empty = Parser $ \_ -> Result $ Left "empty parser"
 
@@ -63,15 +98,15 @@ instance Alternative Parser where
 jsonNullP :: Parser JsonValue
 jsonNullP = (\_ -> JsonNull) <$> stringP "null"
 
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
+
 charP :: Char -> Parser Char
 charP c = Parser go
   where
-    go (x : xs) | x == c = Result $ Right (c, xs)
-    go (s : _ )          = Result . Left $ "The char '"+| s |+"' is not '"+| c |+"'"
-    go []                = Result $ Left "Empty input"
-
-stringP :: String -> Parser String
-stringP = sequenceA . map charP
+    go (x : xs) | x == c = pure (c, xs)
+    go (s : _ )          = failR $ "The char '"+| s |+"' is not '"+| c |+"'"
+    go []                = failR "Empty input"
 
 jsonBoolP :: Parser JsonValue
 jsonBoolP = go <$> (stringP "true" <|> stringP "false")
@@ -80,5 +115,47 @@ jsonBoolP = go <$> (stringP "true" <|> stringP "false")
     go "false" = JsonBool False
     go _       = error "unreachable"
 
+spanP :: (Char -> Bool) -> Parser String
+spanP f = Parser $ pure . span f
+
+readResult :: Read a => String -> Result a
+readResult = Result . readEither
+
+jsonNumberP :: Parser JsonValue
+jsonNumberP = Parser $ \input ->  do
+    (numStr, input') <- runParser (spanP isDigit) input
+    num <- readResult numStr
+    pure (JsonNumber num, input')
+
+jsonStringP :: Parser JsonValue
+jsonStringP = JsonString <$> go
+  where
+    go = charP '"' *> spanP (/= '"') <* charP '"'
+    -- TODO: add handling of escape char
+
+wsP :: Parser String
+wsP = spanP isSpace
+
+commaP :: Parser Char
+commaP = charP ','
+
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep element = go <|> pure []
+  where
+    go = fmap (:) element <*> many (sep *> element)
+
+elemP :: Parser [JsonValue]
+elemP = sepBy (wsP *> commaP <* wsP) jsonValueP
+
+jsonArrayP :: Parser JsonValue
+jsonArrayP = JsonArray <$> go
+  where
+    go = charP '[' *> wsP *> elemP <* wsP <* charP ']'
+
 jsonValueP :: Parser JsonValue
-jsonValueP = jsonNullP
+jsonValueP =
+    jsonNullP
+    <|> jsonBoolP
+    <|> jsonNumberP
+    <|> jsonStringP
+    <|> jsonArrayP
